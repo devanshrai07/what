306dba2ab33fd18b056abf33c5c012bf4aab58d3
Admin <Admin@CHETUIWK1356>
2021-10-22 15:05:20 +0530

    Add project files.

---
+++ CoreCrud/Migrations/20211021130625_m1.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CoreCrud.Migrations
+{
+    public partial class m1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Students",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Salary = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Students", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Students");
+        }
+    }
+}
---
+++ CoreCrud/Models/StudentContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreCrud.Models
+{
+    public class StudentContext : DbContext
+
+    {
+        public StudentContext()
+        {
+
+        }
+        public StudentContext(DbContextOptions<StudentContext> options):base(options)
+        {
+
+        }
+        public DbSet<Student> Students { get; set; }
+    }
+}
